d6a79e6b1d245b414f2a56234f7c354b14b22f23
Frankborty <dessena.franco@gmail.com>
2025-01-17 16:06:06 +0100

    Creato progetto con primeng 19 funzionante

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { FloatLabelModule } from "primeng/floatlabel"
+import { InputTextModule } from 'primeng/inputtext';
+import { DatePickerModule } from 'primeng/datepicker';
+import { FormsModule } from '@angular/forms';
+import { CalendarModule } from 'primeng/calendar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ButtonModule, FloatLabelModule, InputTextModule, DatePickerModule, FormsModule, CalendarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'MyBadPortfolioSite';
+  date: Date | undefined;
 }
