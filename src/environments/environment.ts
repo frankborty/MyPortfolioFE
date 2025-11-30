7d8e31ef9d41d2928bf4f6ba8f4172dd2bb5f8e5
Frankborty <dessena.franco@gmail.com>
2025-11-30 14:20:28 +0100

    Fix Backend URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   //backendUrl: 'http://localhost:5236/api'
   //backendUrl: 'http://localhost:8080/api'
-  backendUrl: 'http://192.168.1.12:8080/api'
+  backendUrl: 'http://docker.tailf149e9.ts.net:8080/api'
 };
